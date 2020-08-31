e11323a0f9e7d50fd008f970ed15cf4688f8bfa3
FelipeAlcocers <FelipeAlcocers@DESKTOP-G9VMQ66>
2020-08-30 21:57:19 -0700

    Primer Proyecto en Java

---
+++ basico_java/src/main/Primer_proyecto.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Primer_proyecto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hola BB, desde java");
+	}
+
+}
